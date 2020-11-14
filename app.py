88fa022bfc4b56350a98c6b06f1fdf81c2d8b751
Pipicallu <thomas.vassallo@gmail.com>
2020-11-14 13:52:32 +0000

    links added properly

--- app.py
+++ app.py
@@ -7,15 +7,3 @@ from werkzeug.security import generate_password_hash, check_password_hash
 if os.path.exists("env.py"):
     import env
 
-
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-    return "We are Halto we are awesome this is our app"
-
-if __name__ == "__main__":
-    app.run(host=os.environ.get("IP"),
-            port=int(os.environ.get("PORT")),
-            # debug must be set to False before deployment
-            debug=True)
